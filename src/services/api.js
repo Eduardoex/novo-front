b9287e96df07b8a089a1ef67f569f742d32145ce
Eduardoex <eduardodelimaex@gmail.com>
2023-10-06 16:52:55 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://rocketnotes-api-ngxw.onrender.com"
+    //baseURL: "https://rocketnotes-api-ngxw.onrender.com"
+    baseURL: "http://localhost:3333/"
 });
 
